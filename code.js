af16a205508d0ee6d9c9500f8067838a644c3047
Mikhail Naganov <mnaganov@chromium.org>
2011-01-23 22:42:02 +0300

    reacts on click

--- code.js
+++ code.js
@@ -1,9 +1,30 @@
+var Mode = {
+  HOME: 1,
+  SHOP: 2,
+  current: 1
+};
+
+function documentClick(event)
+{
+  var element = event.target;
+  if (!element || !element.hasStyleClass("item"))
+    return;
+  var clss = "chosen";
+  if (Mode.current === Mode.HOME) {
+    if (element.hasStyleClass(clss))
+      element.removeStyleClass(clss);
+    else
+      element.addStyleClass(clss);
+  }
+}
+
 function loadItems(items)
 {
   var result = document.createDocumentFragment();
   for (var i = 0, l = items.length; i < l; ++i) {
     var item = document.createElement("div");
     item.textContent = items[i];
+    item.className = "item";
     result.appendChild(item);
   }
   return result;
@@ -19,4 +40,5 @@ function onLoad()
   displayItemsFragmentInList(
     document.getElementById("list"),
     loadItems(items));
+  document.onclick = documentClick;
 }
