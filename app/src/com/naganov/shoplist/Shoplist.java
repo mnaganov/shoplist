1b6b87f94c8d6fdf8d58fea4c5b6172b41174a3c
Mikhail Naganov <mnaganov@chromium.org>
2011-01-26 00:48:09 +0300

    works and reloads

--- app/src/com/naganov/shoplist/Shoplist.java
+++ app/src/com/naganov/shoplist/Shoplist.java
@@ -2,17 +2,45 @@ package com.naganov.shoplist;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MenuInflater;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 
 public class Shoplist extends Activity
 {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+  /** Called when the activity is first created. */
+  @Override
+  public void onCreate(Bundle savedInstanceState)
+  {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.main);
+    WebView myWebView = (WebView) findViewById(R.id.webview);
+    WebSettings webSettings = myWebView.getSettings();
+    webSettings.setJavaScriptEnabled(true);
+    myWebView.clearCache(true);
+    myWebView.loadUrl("http://192.168.1.103/~mnaganov/shoplist/shoplist.html");
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    MenuInflater inflater = getMenuInflater();
+    inflater.inflate(R.menu.menu, menu);
+    return true;
+  }
+  
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    // Handle item selection
+    switch (item.getItemId()) {
+      case R.id.reload:
         WebView myWebView = (WebView) findViewById(R.id.webview);
-        myWebView.loadUrl("http://www.example.com");
+        myWebView.clearCache(true);
+        myWebView.reload();
+        return true;
+      default:
+        return super.onOptionsItemSelected(item);
     }
+  }  
 }
